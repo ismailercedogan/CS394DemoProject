acddef644af1399d5927c7ff3f4c4d99e06fbcf3
ismailercedogan <ismail.ercedogan@ozu.edu.tr>
2023-01-16 09:31:26 +0300

    navigation menu updated

--- app/src/main/java/com/example/cs394demoproject/util/Constant.kt
+++ app/src/main/java/com/example/cs394demoproject/util/Constant.kt
@@ -2,7 +2,7 @@ package com.example.cs394demoproject.util
 
 class Constants {
     companion object {
-        const val API_KEY = "3d2116ed1bad4d6fb38c51aec2449a9d"
+        const val API_KEY = "6597d7f810be4154827feab4984d0e3e"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
